eb17e7b07fbe4163af688e8edd5c9156675bc292
Peuly <peulype@gmail.com>
2023-05-14 23:00:49 -0300

    Creados los modelos y agregados a la base de datos - sin entorno virtual

---
+++ club_once_estrellas/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+# Create your models here.
+class Socios(models.Model):
+    nombre = models.CharField(max_length=64)  # Equivalente de str
+    apellido = models.CharField(max_length=64)
+    cedula_de_identidad = models.CharField(max_length=8)
+    direccion = models.CharField(max_length=64)
+    celular = models.CharField(max_length=20)
+    numero_socio = models.CharField(max_length=128)
+    es_socio = models.BooleanField(default=False) # Booleano que me dice si es o no socio. 
+
+class Actividades(models.Model):
+    actividad = models.CharField(max_length=64)
+    horario = models.TimeField()
+    dia = models.IntegerField()
+    nombre_profesor = models.CharField(max_length=64)
+    telefono_contacto = models.CharField(max_length=20)
+    
+class Salones(models.Model):
+    tipo = models.CharField(max_length=64)
+    horario = models.TimeField()
+    precio = models.IntegerField()
--- primer_proyecto/settings.py
+++ primer_proyecto/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'club_once_estrellas',
 ]
 
 MIDDLEWARE = [
